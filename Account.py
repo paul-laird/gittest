3327eba5c9f8a309d32082c3d9f2afcf226c8713
89382 <89382@CTX-XA-VDA172>
2020-02-17 09:56:41 +0000

    first code commit

---
+++ Account.py
@@ -0,0 +1,138 @@
+
+# coding: utf-8
+
+# In[1]:
+
+#Create a Bank Account class
+
+#define functions deposit, withdraw, transfer, checkBalance
+
+#Create some bank accounts and test your methods
+
+#Try to prevent illegal actions on the accounts
+
+#github.com/sadfjkhsdafk/sadkygsfdak
+
+
+# In[ ]:
+
+#Update: The below is unfinished, and ignore pin-related stuff
+
+#Also: Modify this to create a Wallet class 
+#which can handle multiple simultaneous balances
+#in different currencies or securities
+
+
+# In[40]:
+
+import random
+import hashlib
+class BankAccount:
+    Accounts={}
+    def __init__ (self, name,pin):
+        self.__name=name
+        self.__balance=0
+        m = hashlib.sha256()
+        m.update(pin.encode())
+        self.__pin=m.digest()
+        self.__accNo=random.randint(10000000,99999999)
+        while self.__accNo in BankAccount.Accounts:
+            self.__accNo=random.randint(10000000,99999999)
+        BankAccount.Accounts[self.__accNo]=self
+    def getDetails(self):
+        return {'name':self.__name}
+    def transfer(self,amt,dest):
+        if self.withdraw(amt):
+            dest.deposit(amt)
+            return True
+        else:
+            return False
+    def withdraw(self,amt,pin):
+        m = hashlib.sha256()
+        m.update(pin.encode())
+        if m.digest()!=self.__pin:
+            raise ValueError('Wrong PIN')
+        if amt>self.__balance:
+            return False
+        else:
+            self.__balance-=amt
+            return True
+    def deposit(self,amt):
+        self.__balance+=amt
+
+
+# In[41]:
+
+a=BankAccount('Paul','1sdfhnjdgj,ketdyshjsrfx234')
+a.deposit(100)
+try:
+    a.withdraw(50,'1233')
+except ValueError as v:
+    print (v)
+    print (a._BankAccount__pin)
+
+
+# In[27]:
+
+BankAccount.Accounts.keys()
+for k,v in BankAccount.Accounts.items():
+    print(' %i: %s : %i'%(k,v.getDetails(), v._BankAccount__balance))
+
+
+# In[26]:
+
+BankAccount.Accounts[11566923].transfer(-1000,BankAccount.Accounts[47553964])
+
+
+# In[ ]:
+
+
+
+
+# In[28]:
+
+menu=input('''What operation would you like to perform:
+Deposit, Withdraw, Transfer, Check Balance''')
+if menu=='Transfer':
+    src=int(input('Enter your account number: '))
+    dst=int(input('Enter the recipient\'s account number: '))
+    amt=int(input('Enter the amount: '))
+    if src in BankAccount.Accounts and dst in BankAccount.Accounts:
+        BankAccount.Accounts[src].transfer(amt,BankAccount.Accounts[dst])
+    else:
+        print('Error!')
+
+
+# In[ ]:
+
+
+
+
+# In[16]:
+
+flag=True
+while flag:
+    flag=False
+    name=input('Whose account shall we create? ')
+    try:
+        BankAccount(name)
+    except ValueError:
+        print ('Name Taken, please try again!')
+        flag=True
+    
+
+
+# In[18]:
+
+BankAccount.Accounts.keys()
+
+
+# In[19]:
+
+BankAccount.Accounts[12631226].getDetails()
+
+
+# In[ ]:
+
+
+
